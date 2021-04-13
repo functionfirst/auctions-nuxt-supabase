63e45a7727258d07f20b062236ac26628f4bac84
Alan Jenkins <alanjenkins1978@gmail.com>
2021-04-13 18:28:50 +0100

    Rename burger to menu and use new icon

--- components/Layout/MenuIcon.vue
+++ components/Layout/MenuIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sm:hidden mt-4">
     <button class="h-6 w-6">
-      <IconBurger />
+      <IconMenu />
     </button>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <LayoutHeader>
       <LayoutHeaderLogo />
-      <LayoutBurgerMenu />
+      <LayoutMenuIcon />
       <LayoutNavigation />
       <LayoutUserNav />
     </LayoutHeader>
