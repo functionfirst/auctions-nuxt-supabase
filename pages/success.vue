78e536d4edb33446a6174e2fd75c03af59ca88d6
Alan Jenkins <alanjenkins1978@gmail.com>
2021-04-17 23:38:28 +0100

    Remove login button after successful signup
    This button is redundant since the user is required to click the confirmation email link and when doing so they will be automatically logged in using their session.

--- pages/success.vue
+++ pages/success.vue
@@ -7,14 +7,5 @@
     <p class="my-4 text-gray-600">
       You should receive a confirmation email shortly, please use the link in this email to activate your account.
     </p>
-
-    <p class="text-center">
-      <nuxt-link
-        to="/login"
-        class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-      >
-        Login
-      </nuxt-link>
-    </p>
   </div>
 </template>
