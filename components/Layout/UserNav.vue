95723cc1d64ed0a1bca210d395b9ce6c2d8d23af
Alan Jenkins <alanjenkins1978@gmail.com>
2021-04-17 23:22:21 +0100

    Update user component style and include a user icon
    The user icon replaces hard-coded avatar user initials.

--- components/Layout/UserNav.vue
+++ components/Layout/UserNav.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="justify-end md:flex-1 lg:w-0 text-xs hidden sm:flex flex-auto items-center">
+  <div class="justify-end md:flex-1 lg:w-0 hidden sm:flex flex-auto items-center">
     <div
       v-show="user && user.email"
       class="ml-3 relative flex items-center"
     >
-      <p class="px-4 text-gray-500">
+      <p class="px-4 text-gray-500 text-xs">
         Logged in as {{ user && user.email }}
       </p>
 
@@ -12,13 +12,13 @@
         <button
           id="profleMenuButton"
           type="button"
-          class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+          class="h-10 rounded-full w-10 flex items-center bg-indigo-600 text-white p-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
           aria-expanded="false"
           aria-haspopup="true"
           @click="show = !show"
         >
           <span class="sr-only">Open user menu</span>
-          <div class="flex items-center justify-center bg-indigo-600 text-white rounded-full h-8 w-8">AJ</div>
+          <IconUser />
         </button>
 
         <transition
