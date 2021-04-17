ee7899c89269b5a2ca541d14a23f4d62af763ad1
Alan Jenkins <alanjenkins1978@gmail.com>
2021-04-17 13:13:09 +0100

    Display logged in user details on account page (temporary)

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    User account (authenticated)
+    <p class="text-gray-500 mb-2">
+      User account (authenticated)
+    </p>
+
+    {{ user.email }}
   </div>
 </template>
 
 <script>
 export default {
   middleware: 'authenticated',
+
+  computed: {
+    user () {
+      return this.$store.state.auth.user
+    }
+  }
 }
 </script>
