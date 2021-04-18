9eaa770014f23e68e3722f23bb39d9d6dc9239de
Alan Jenkins <alanjenkins1978@gmail.com>
2021-04-18 11:54:26 +0100

    Simplify navigation style
    Removes dropdown menu for logged in users
    
    Make nav and top bar links lowercase to improve readability.
    
    Add icons to user navigation buttons.

--- components/Layout/Navigation.vue
+++ components/Layout/Navigation.vue
@@ -1,10 +1,10 @@
 <template>
-  <nav class="hidden justify-start lg:flex space-x-4 uppercase text-xs font-medium">
+  <nav class="hidden justify-start lg:flex space-x-4 text-sm font-medium">
     <nuxt-link
       v-for="(link, index) in links"
       :key="index"
       :to="link.to"
-      class="text-gray-500 hover:text-indigo-800 px-3 py-3"
+      class="text-gray-500 hover:text-indigo-800 px-3 py-2"
     >
       {{ link.name }}
     </nuxt-link>
--- components/Layout/UserNav.vue
+++ components/Layout/UserNav.vue
@@ -2,77 +2,42 @@
   <div class="justify-end md:flex-1 lg:w-0 hidden sm:flex flex-auto items-center">
     <div
       v-show="user && user.email"
-      class="ml-3 relative flex items-center"
+      class="ml-3 relative flex items-center xuppercase tracking-wider font-semibold text-sm"
     >
-      <p class="px-4 text-gray-500 text-xs">
-        Logged in as {{ user && user.email }}
-      </p>
+      <nuxt-link
+        to="/account"
+        class="flex items-center px-4 rounded py-2 text-gray-500 hover:text-indigo-800 mr-2"
+      >
+        <IconUserCircle class="w-6 h-6" />
+        Your Account
+      </nuxt-link>
 
-      <div class="relative">
-        <button
-          id="profleMenuButton"
-          type="button"
-          class="h-10 rounded-full w-10 flex items-center bg-indigo-600 text-white p-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-          aria-expanded="false"
-          aria-haspopup="true"
-          @click="show = !show"
-        >
-          <span class="sr-only">Open user menu</span>
-          <IconUser />
-        </button>
-
-        <transition
-          enter-active-class="transition ease-out duration-100"
-          enter-from-class="transform opacity-0 scale-95"
-          enter-to-class="transform opacity-100 scale-100"
-          leave-active-class="transition ease-in duration-75"
-          leave-from-class="transform opacity-100 scale-100"
-          leave-to-class="transform opacity-0 scale-95"
-        >
-          <div
-            v-show="show"
-            class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="profleMenuButton"
-            tabindex="-1"
-          >
-            <nuxt-link
-              to="/account"
-              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              role="menuitem"
-              tabindex="-1"
-              @click.native="show = false"
-            >
-              Your Profile
-            </nuxt-link>
-
-            <nuxt-link
-              to="/logout"
-              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              role="menuitem"
-              tabindex="-1"
-              @click.native="show = false"
-            >
-              Logout
-            </nuxt-link>
-          </div>
-        </transition>
-      </div>
+      <nuxt-link
+        to="/logout"
+        class="flex items-center px-4 py-2 text-gray-500 hover:text-indigo-800 mr-2"
+      >
+        <IconLogout class="w-6 h-6" />
+        Logout
+      </nuxt-link>
     </div>
 
-    <div v-show="!user">
+    <div
+      v-show="!user"
+      class="flex items-center tracking-wider font-semibold text-sm"
+    >
       <nuxt-link
         to="/login"
-        class="px-6 py-4 uppercase tracking-wider font-semibold text-xs text-gray-500 hover:text-indigo-800 mr-2"
+        class="flex items-center px-4 py-2 text-gray-500 hover:text-indigo-800 mr-2"
       >
+        <IconLock class="w-6 h-6 mr-1" />
         Log In
       </nuxt-link>
 
       <nuxt-link
         to="/register"
-        class="px-12 py-4 bg-indigo-500 hover:bg-indigo-400 rounded-full uppercase tracking-wider font-semibold text-white text-xs"
+        class="flex items-center px-6 py-2 bg-indigo-500 hover:bg-indigo-400 rounded-full text-white"
       >
+        <IconUserCircle class="mr-1 w-6 h-6" />
         Sign Up
       </nuxt-link>
     </div>
@@ -81,12 +46,6 @@
 
 <script>
 export default {
-  data () {
-    return {
-      show: false
-    }
-  },
-
   computed: {
     user () {
       return this.$store.state.user
