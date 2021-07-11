40238114752f72378e91587eab598100a87ccda1
Alan Jenkins <alan@functionfirst.co.uk>
2021-07-11 19:12:03 +0100

    Update supabase plugin to export client

--- plugins/supabase.ts
+++ plugins/supabase.ts
@@ -1,10 +1,12 @@
 import { defineNuxtPlugin } from '@nuxtjs/composition-api'
 import { createClient } from '@supabase/supabase-js'
 
-export default defineNuxtPlugin((_context, inject) => {
-  const URL = process.env.NUXT_ENV_SUPABASE_URL as string
-  const KEY = process.env.NUXT_ENV_SUPABASE_KEY as string
-  const supabase = createClient(URL, KEY)
+const URL = process.env.NUXT_ENV_SUPABASE_URL as string
+const KEY = process.env.NUXT_ENV_SUPABASE_KEY as string
 
+export const supabase = createClient(URL, KEY)
+
+// @todo do we need this?
+export default defineNuxtPlugin((_context, inject) => {
   inject('supabase', supabase)
 })
