bfb6454f4481f43b01d8a5a3663252ceb6668918
Alan Jenkins <alan@functionfirst.co.uk>
2021-11-04 16:18:50 +0000

    Removes unused classx attribute

--- components/BaseInput.vue
+++ components/BaseInput.vue
@@ -3,7 +3,6 @@
     :type="props.type || 'text'"
     :value="props.value"
     class="block border border-gray-300 w-full text-gray-700 rounded-sm py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-300"
-    classx="focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
     v-bind="data.attrs"
     @input="listeners.input && listeners.input($event.target.value)"
   >
