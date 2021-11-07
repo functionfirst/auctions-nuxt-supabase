408a467c5e5e2c7c0d0f488b7d8aa1712a7467fb
Alan Jenkins <alan@functionfirst.co.uk>
2021-11-07 10:35:00 +0000

    Add consistent margin for form inputs

--- components/BaseInput.vue
+++ components/BaseInput.vue
@@ -2,7 +2,7 @@
   <input
     :type="props.type || 'text'"
     :value="props.value"
-    class="block border border-gray-300 w-full text-gray-700 rounded-sm py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-300"
+    class="block border border-gray-300 w-full text-gray-700 rounded-sm py-3 px-4 mb-6 leading-tight focus:bg-white focus:border-gray-300"
     v-bind="data.attrs"
     @input="listeners.input && listeners.input($event.target.value)"
   >
