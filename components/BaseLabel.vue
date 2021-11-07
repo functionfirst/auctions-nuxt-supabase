565a191c86a50feb28b8fa4b6e867b24cc424f18
Alan Jenkins <alan@functionfirst.co.uk>
2021-11-07 10:34:50 +0000

    Fix margin issue due to display property

--- components/BaseLabel.vue
+++ components/BaseLabel.vue
@@ -1,6 +1,6 @@
 <template functional>
   <label
-    class="flex-inline cursor-pointer uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+    class="inline-block cursor-pointer uppercase tracking-wide text-gray-700 text-xs font-bold mb-1"
     :class="[
       data.class,
       data.staticClass
