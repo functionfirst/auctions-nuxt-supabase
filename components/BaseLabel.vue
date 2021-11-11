366758994aa8a444b8aaf66082f45f302ed59f05
Alan Jenkins <alan@functionfirst.co.uk>
2021-11-11 10:45:39 +0000

    Simplify how we set css on BaseLabel

--- components/BaseLabel.vue
+++ components/BaseLabel.vue
@@ -1,16 +1,7 @@
-<template functional>
+<template>
   <label
-    class="inline-block cursor-pointer uppercase tracking-wide text-gray-700 text-xs font-bold mb-1"
-    :class="[
-      data.class,
-      data.staticClass
-    ]"
-    :style="[
-      data.style,
-      data.staticStyle
-    ]"
-    v-bind="data.attrs"
-    v-on="listeners"
+    class="inline-block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1"
+    :class="$attrs.for ? 'cursor-pointer' : ''"
   >
     <slot />
   </label>
