7626ca441128f6b5a46838c8dc23f1101d52a837
Alan Jenkins <alan@functionfirst.co.uk>
2021-11-11 11:18:30 +0000

    Update page widths

--- pages/discover.vue
+++ pages/discover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="max-w-6xl mx-auto px-4 sm:px-6">
     <h1 class="font-semibold text-2xl mb-4">
       Discover
     </h1>
--- pages/my-auctions.vue
+++ pages/my-auctions.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>
+  <div class="max-w-6xl mx-auto px-4 sm:px-6">
+    <h1 class="font-semibold text-2xl mb-4">
       My Auctions
     </h1>
   </div>
