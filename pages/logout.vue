013729d69ce02173d80d0022e57ff26d36e04c9a
Alan Jenkins <alan@functionfirst.co.uk>
2022-01-18 14:44:46 +0000

    Add cypress dev dep

--- pages/logout.vue
+++ pages/logout.vue
@@ -5,14 +5,26 @@
 </template>
 
 <script lang="ts">
-import { onMounted, defineComponent } from '@nuxtjs/composition-api'
-import useAuth from '@/composables/useAuth'
+import { onMounted, defineComponent, useContext, useRouter, ref } from '@nuxtjs/composition-api'
+import AuthService from '../services/AuthService'
 
 export default defineComponent({
   layout: 'base',
 
   setup () {
-    const { error, signout } = useAuth()
+    const router = useRouter()
+    const { $supabase } = useContext()
+    const service = new AuthService($supabase)
+    const error = ref('')
+    const signout = async () => {
+      const { error: signoutError } = await service.signout()
+
+      if (signoutError) {
+        error.value = signoutError
+      } else {
+        router.push('/')
+      }
+    }
 
     onMounted(() => {
       signout()
