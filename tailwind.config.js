b0126edf66f1301622114747feb60acb49e7b2e5
Alan Jenkins <alan@functionfirst.co.uk>
2022-01-25 10:51:16 +0000

    Use singlequotes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
   plugins: [],
   purge: {
     content: [
-      `components/**/*.{vue,js}`,
-      `layouts/**/*.vue`,
-      `pages/**/*.vue`,
-      `plugins/**/*.{js,ts}`,
-      `nuxt.config.{js,ts}`
+      'components/**/*.{vue,js}',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.{js,ts}',
+      'nuxt.config.{js,ts}'
     ]
   }
 }
